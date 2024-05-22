33cbfd9d7d9cfbb6cd39510e9fe79a95ef41addc
SunTao <1027353579@qq.com>
2024-05-23 00:57:10 +0800

    增加全量工具模块

---
+++ muses-utils/muses-utils-all/src/main/java/com/github/stazxr/muses/utils/all/App.java
@@ -0,0 +1,13 @@
+package com.github.stazxr.muses.utils.all;
+
+/**
+ * 全量工具包
+ *
+ * @author SunTao
+ * @since 2024-05-20
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("缪斯全量工具包");
+    }
+}
