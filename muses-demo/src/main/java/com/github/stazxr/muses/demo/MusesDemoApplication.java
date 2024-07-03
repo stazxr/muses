4e430b9c1523a55d09196e935f94dadc4dc77f98
SunTao <1027353579@qq.com>
2024-07-03 20:52:47 +0800

    增加demo模块

---
+++ muses-demo/src/main/java/com/github/stazxr/muses/demo/MusesDemoApplication.java
@@ -0,0 +1,17 @@
+package com.github.stazxr.muses.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Muses 框架使用样例
+ *
+ * @author SunTao
+ * @since 2024-05-10
+ */
+@SpringBootApplication
+public class MusesDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MusesDemoApplication.class, args);
+    }
+}
