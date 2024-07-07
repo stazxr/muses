e6396449e66735f9a84757d83e8d9383418e1b00
SunTao <1027353579@qq.com>
2024-07-08 00:04:47 +0800

    新增 muses spring web starter 模块

---
+++ muses-base/muses-spring-boot-web-starter/src/main/java/com/github/muses/stater/App.java
@@ -0,0 +1,13 @@
+package com.github.muses.stater;
+
+/**
+ * muses spring boot web starter
+ *
+ * @author SunTao
+ * @since 2024-07-07
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("muses spring boot web starter version: 0.0.1-SNAPSHOT");
+    }
+}
